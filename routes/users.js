6316acf2969ee017b63b1d61de104eebb1c942a7
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-05 13:23:04 -0700

    wip: required the users controllers and ensuredLoggIn middleware, and add a router to show the user their profile page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const usersCtrl = require('../controllers/users');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/:id', ensureLoggedIn, usersCtrl.show)
+
 
 module.exports = router;
