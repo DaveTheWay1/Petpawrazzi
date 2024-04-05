12862fd9d659bd7c3c886825c18ab2215da55394
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-05 14:04:00 -0700

    created a pet model including a reference to the user model

---
+++ models/pet.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const petSchema = new Schema({
+    owner:{type:Schema.Types.ObjectId, ref:'User'},
+    name:{type:String},
+    age:{type:Number},
+    gender:{type:String},
+    birthday: Date
+}, {timestamps: true})
+
+module.exports = mongoose.model('Pet', petSchema);
