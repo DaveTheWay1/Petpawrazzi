6eceb114177d2e76e30fcc4d0abfb368fb262377
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-05 19:59:53 -0700

    added a route to handle the show functionality to display one pet

--- routes/pets.js
+++ routes/pets.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const petsCtrl = require('../controllers/pets')
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-router.get('/pets/new', ensureLoggedIn, petsCtrl.new)
-router.post('/pets/:id', ensureLoggedIn, petsCtrl.create)
+router.get('/pets/new', ensureLoggedIn, petsCtrl.new);
+router.post('/pets/:id', ensureLoggedIn, petsCtrl.create);
+router.get('/pets/:id', ensureLoggedIn, petsCtrl.show);
 
 module.exports = router;
