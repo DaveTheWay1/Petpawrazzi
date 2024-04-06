c97e49c0fd799efb9d7d003367ecfd7e0b92bb85
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-05 19:22:51 -0700

    required user model and created a create to handle the submission of the new pet form

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,8 +1,19 @@
 const Pet = require('../models/pet');
+const User = require('../models/user')
 
 module.exports = {
-    new:addPet
+    new:addPet,
+    create
 }
 async function addPet(req,res){
+    res.render('pets/new')
+}
+
+async function create(req,res){
+    const pet = await Pet.create(req.body);
+    const pets = await Pet.find({})
+    const user = await User.findById(pet.owner)
+    await pet.save();
+    console.log(user.name)
     res.render('pets/new')
 }
