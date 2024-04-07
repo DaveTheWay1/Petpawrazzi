3ee7791d7e470467bc01b796b0741628be92991f
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-06 22:07:43 -0700

    added routes for posts and mounted it

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ require('./config/passport');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var petsRouter = require('./routes/pets');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -47,6 +48,7 @@ app.use(function (req, res, next) {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', petsRouter);
+app.use('/', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
