db5cb090a4fe43f188329b0542a6778860e7fc6f
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-06 22:12:30 -0700

    minor edits to code, incorrect lowercases cause a bug but now fixed

--- models/post.js
+++ models/post.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    img:{type:String, required:true},
-    content:{type:String},
-    petName:{type:Schema.type.ObjectId, ref: 'Pet'},
-    author:{type:Schema.type.ObjectId, ref: 'Pet'},
+    img:{type:String},
+    caption:{type:String},
+    petName:{type:Schema.Types.ObjectId, ref: 'Pet'},
+    author:{type:Schema.Types.ObjectId, ref: 'Pet'},
     likes:{type:Number, default:0}
 }, {timestamps:true});
 
