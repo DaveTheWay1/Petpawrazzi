1708496c83e828dcae458e100db31bbba2b194d3
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-08 14:24:05 -0700

    no changes here though its indicating there is but all there is a route to show single user profile

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const usersCtrl = require('../controllers/users');
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-router.get('/:id', ensureLoggedIn, usersCtrl.show)
+router.get('/:id', ensureLoggedIn, usersCtrl.show);
 
 
 module.exports = router;
