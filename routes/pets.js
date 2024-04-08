ad89e0992b01948ec97a234002c8959373a4ada4
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-08 14:11:55 -0700

    added a route for delete functionality

--- routes/pets.js
+++ routes/pets.js
@@ -6,5 +6,6 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 router.get('/pets/new', ensureLoggedIn, petsCtrl.new);
 router.post('/pets/:id', ensureLoggedIn, petsCtrl.create);
 router.get('/pets/:id', ensureLoggedIn, petsCtrl.show);
+router.delete('/pets/:id', ensureLoggedIn, petsCtrl.delete);
 
 module.exports = router;
