751fc3c2e44030c4a438929895f3c41f39f446de
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-08 16:38:47 -0700

    implemented the ensure loggedin middleware to the edit route, and created a route for updating

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,7 @@ router.post('/posts/create',ensureLoggedIn ,postsCtrl.create);
 router.get('/pets/:id/posts/new', ensureLoggedIn, postsCtrl.new);
 router.get('/post/:id', ensureLoggedIn, postsCtrl.show)
 router.delete('/post/:id', ensureLoggedIn, postsCtrl.delete)
-router.get('/post/:id/edit', postsCtrl.edit);
+router.get('/post/:id/edit',ensureLoggedIn, postsCtrl.edit);
+router.put('/post/:id',ensureLoggedIn, postsCtrl.update);
 
 module.exports = router;
