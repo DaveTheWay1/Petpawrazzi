5790fcf69f215d9879673dedfb3164421eb8321f
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-12 14:05:19 -0700

    working on heroku solution

--- config/database.js
+++ config/database.js
@@ -1,10 +1,32 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.DATABASE_URL);
+	
+// // shortcut to mongoose.connection object
+// const db = mongoose.connection;
+	
+// db.on('connected', function() {
+//     console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+// });
+
+const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL);
-	
-// shortcut to mongoose.connection object
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 const db = mongoose.connection;
-	
-db.on('connected', function() {
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
     console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
+
+// Start the web server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
